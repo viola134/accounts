22d5b8eaa38a33cd4b2785cfff8543016a156b82
viola134 <violapakhomova13@gmail.com>
2022-09-25 15:07:06 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+$users1 = ["John" => "qwerty", "Nicole" => "asdf", "Mark" => "ww"];
+$users2 = ["Joan" => "1234", "Mark" => "poiu", "Nicole" => "ggg"];
+print_r(array_merge($users1, $users2));
